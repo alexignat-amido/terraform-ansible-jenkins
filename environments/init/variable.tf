df757695c4c4b9441dd9c88e7c2ed25438873c5e
alexignat-amido <alex.ignat@amido.com>
2019-02-25 11:03:14 +0000

    Deleting the aws access and secret key

--- environments/init/variable.tf
+++ environments/init/variable.tf
@@ -3,10 +3,10 @@ variable "region" {
     default = "eu-west-1"
 }
 
-variable "aws_access_key" {
-    description = "Provide aws access key"
-}
+# variable "aws_access_key" {
+#     description = "Provide aws access key"
+# }
 
-variable "aws_secret_key" {
-    description = "Provide aws secret key"
-}
+# variable "aws_secret_key" {
+#     description = "Provide aws secret key"
+# }
