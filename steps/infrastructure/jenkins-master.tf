c35ae1b9bcfe4ba8233b525374753824dd036b1f
alexignat-amido <alex.ignat@amido.com>
2019-02-25 16:07:17 +0000

    changing the Jenkins-master Configuration

--- steps/infrastructure/jenkins-master.tf
+++ steps/infrastructure/jenkins-master.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "jenkins-master" {
     ami = "${var.aws_linux_ami}"
     instance_type = "t2.micro"
     subnet_id = "${aws_subnet.public-subnet.id}"
-    security_groups = ["${aws_security_group.default.id}"]
+    # security_groups = ["${aws_security_group.default.id}"]
     vpc_security_group_ids = ["${aws_security_group.default.id}"]
     key_name = "jenkins-key"
     associate_public_ip_address = true
