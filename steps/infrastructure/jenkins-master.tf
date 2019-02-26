a3a94a62420073da198c512df8725830b80eab99
alexignat-amido <alex.ignat@amido.com>
2019-02-26 11:05:47 +0000

    user data file configuration

--- steps/infrastructure/jenkins-master.tf
+++ steps/infrastructure/jenkins-master.tf
@@ -1,6 +1,4 @@
-data "template_file" "jenkins_userdata" {
-  template = "${file("userdata.tpl")}"
-}
+
 resource "aws_instance" "jenkins-master" {
     ami = "${var.aws_linux_ami}"
     instance_type = "t2.micro"
@@ -11,7 +9,7 @@ resource "aws_instance" "jenkins-master" {
     associate_public_ip_address = true
     source_dest_check           = true
     depends_on = ["aws_internet_gateway.default"]
-    user_data                   = "${data.template_file.jenkins_userdata.rendered}"
+    user_data                   = "${file("userdata.tpl")}"
     tags = {
         Name = "jenkins-master"
     }
