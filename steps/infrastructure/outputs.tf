1ea2ff5630f0c18845d3c8ae1c9ed907f2a01bdd
alexignat-amido <alex.ignat@amido.com>
2019-02-28 14:47:18 +0000

    try to control the output

--- steps/infrastructure/outputs.tf
+++ steps/infrastructure/outputs.tf
@@ -1,7 +1,11 @@
-output "jenkins-url" {
+output "[hosts]" {
   value = "${aws_instance.jenkins-master.public_ip}"
 }
 
 # output "ssh-jenkins" {
 #   value =  "ssh -i 'jenkins-key.pem' ec2-user@${aws_instance.jenkins-master.public_dns}"
+# }
+
+# output "invoke-url" {
+#     value = "https://${aws_api_gateway_deployment.ddtdeployment.rest_api_id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_deployment.ddtdeployment.stage_name}/${aws_lambda_function.data_source_ddb.function_name}"
 # }
