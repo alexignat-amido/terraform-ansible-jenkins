998ee487bdddab2e919f7a34901cebafcfb1f30e
alexignat-amido <alex.ignat@amido.com>
2019-02-28 09:54:11 +0000

    Commit Changes for outputs.tf

--- steps/infrastructure/outputs.tf
+++ steps/infrastructure/outputs.tf
@@ -1,7 +1,7 @@
 output "jenkins-url" {
-  value = "http://${aws_instance.jenkins-master.public_ip}:8080"
+  value = "${aws_instance.jenkins-master.public_ip}"
 }
 
-output "ssh-jenkins" {
-  value =  "ssh -i 'jenkins-key.pem' ec2-user@${aws_instance.jenkins-master.public_dns}"
-}
+# output "ssh-jenkins" {
+#   value =  "ssh -i 'jenkins-key.pem' ec2-user@${aws_instance.jenkins-master.public_dns}"
+# }
